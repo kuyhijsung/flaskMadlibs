accb26ec3b5c8e76545520c916492b2d6afe05a3
Andy <jihyuk.sung@gmail.com>
2021-05-01 13:26:38 -0600

    Version 1 Finished

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
-from stories import generate
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "test123"
+
+debug = DebugToolbarExtension(app)
 
 
 @app.route("/")
-def show_home():
-    return "Welcome"
+def render_home():
+    questions = story.prompts
+    return render_template("form.html", questions=questions)
+
+
+@app.route("/story")
+def render_story():
+    madlibs = story.generate(request.args)
+    return render_template("madlibs.html", madlibs=madlibs)
